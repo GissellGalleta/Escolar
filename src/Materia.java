7ae5157751d31d19fa11a4394e4ee90b12c784de
Karol <L21020442@veracruz.tecnm.mx>
2023-02-28 17:52:53 -0600

    Codigo Materia

---
+++ src/Materia.java
@@ -0,0 +1,30 @@
+public class Materia {
+    public String nombre;
+    public  int id;
+
+    public Materia(){
+
+    }
+    public Materia(String nombre, int id){
+        this.id=id;
+        this.nombre =nombre;
+
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNumeroId(int id) {
+        this.id = id;
+    }
+}
