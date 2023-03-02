96e8a492e5232399faeb46569337e45a68e03cbc
nadia <L21021180@veracruz.tecnm.mx>
2023-03-01 23:03:39 -0600

    <type> [optional scope]: <create class Alumno new>

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,6 +1,6 @@
 public class Alumno {
     int numControl;
-    String Nombre;
+    String Nombres;
 
     //Especialidad especialidad;
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class main {
+public class Main {
 
     public static Scanner teclado=new Scanner(System.in);
     public static void main(String[] args) {
