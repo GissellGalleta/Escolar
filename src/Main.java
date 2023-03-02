ac919256265a2598eeeac54970028e215160f019
gissell <l21020451@veracruz.tecnm.mx>
2023-03-02 11:38:08 -0600

    class grupo and main complete

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,127 @@
+//import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Vector;
+
+
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
+    public static Scanner text = new Scanner(System.in);
+    static int aid = 0;
+    static String nombre;
 
-    public static Scanner teclado =new Scanner(System.in);
+    public static Vector<Grupo> grupos = new Vector<Grupo>();
+    public static Vector<Especialidad> especialidades = new Vector<Especialidad>();
+    public static Vector<Alumno> alumnos = new Vector<Alumno>();
+    public static Vector<Catedratico> catedraticos = new Vector<Catedratico>();
+    public static Vector<Materia> materias = new Vector<Materia>();
+
+    Especialidad especialidadObj = new Especialidad();
+
     public static void main(String[] args) {
-
-        int op1 = menu();
+        int opc = 0;
         do {
-            switch (op1) {
-                case 1:
+            opc = menu();
 
+            switch (opc) {
+                case 1:
+                    altaEspecialidad();
                     break;
                 case 2:
-
+                    altaAlumno();
                     break;
                 case 3:
-
+                    altaCatedratico();
                     break;
                 case 4:
+                    altaMateria();
+                    break;
+                case 5:
+                    altaGrupo();
+                    break;
+                case 6:
+                    System.out.println("\nElija que registro quiere ver\n");
+
+                    System.out.println("1.- Especialidad");
+                    System.out.println("2.- Alumno");
+                    System.out.println("3.- Catedratico");
+                    System.out.println("4.- Materia ");
+                    System.out.println("5.- Grupo");
+                    opc = scan.nextInt();
+
+                    switch (opc) {
+                        case 1:
+                            impEsp();
+                            System.out.println("\n");
+                            break;
+
+                        case 2:
+                            impAlu();
+                            System.out.println("\n");
+                            break;
+
+                        case 3:
+                            impCat();
+                            System.out.println("\n");
+                            break;
+
+                        case 4:
+                            impMat();
+                            System.out.println("\n");
+                            break;
+
+                        case 5:
+                            impGru();
+                            System.out.println("\n");
+                            break;
+                    }
 
                     break;
-
-                case 5:
-
+                case 7:
+                    System.out.println("Bye");
+                    break;
+                default:
+                    System.out.println("...Opcion no valida !");
                     break;
             }
+        } while (opc != 7);
 
-        }while (op1!=4);
     }
+
     public static int menu() {
-        System.out.println("Bienvenido a la sistemas de Altas Escolar");
-        System.out.println("seleccione una categoria de su interes.");
-
-        System.out.println("1 - Alta especialidad");
-        System.out.println("2 - Alta estudiante");
-        System.out.println("3 - Alta Catedratico");
-        System.out.println("4 - Alta materia");
-        System.out.println("5 - Alta grupo");
-        System.out.println("6 - Salir");
-        return teclado.nextInt();
+        int opcion = 0;
+        System.out.println("\nMENU PRINCIPAL\n");
+        System.out.println("1.- Alta especialidad");
+        System.out.println("2.- Alta alumno");
+        System.out.println("3.- Alta catedratico");
+        System.out.println("4.- Alta materia ");
+        System.out.println("5.- Alta grupo");
+        System.out.println("6.- Desplegar registros");
+        System.out.println("7.- Salir");
+        System.out.print("\n Seleccione la opcion deseada: ");
+        opcion = scan.nextInt();
+        return opcion;
     }
-
-    String grupo;
-    public String getGrupo() {
-        return grupo;
+    // - - - - - - - - - - - - -
+    public static void altaGrupo(){
+        String clave;
+        int hora;
+        int salon;
+        System.out.println("\n- Alta Grupo -\n");
+        System.out.print("Clave del grupo: ");
+        clave=scan.nextLine();
+        System.out.print("Horario del grupo: ");
+        hora=text.nextInt();
+        System.out.println("Salon:");
+        salon=text.nextInt();
+        grupos.addElement(new Grupo(clave, hora, salon));
     }
-
-    public void setGrupo(String grupo){
-        this.grupo=grupo;
+    public static void impGru(){
+        System.out.println("\n=== Registro de Grupo ===\n");
+        for(int i=0; i<grupos.size(); i++){
+            grupos.elementAt(i).imprimir();
+        }
     }
+    // - - - - - - - - - - - - -
 
 }
